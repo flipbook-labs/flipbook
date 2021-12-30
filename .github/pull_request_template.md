52a82d026dd7a9fddbfb7205cce3c06090587ccc
vocksel <voxeldavid@gmail.com>
2021-12-30 18:23:29 -0500

    Setup workflow for developing the plugin (#1)
    * Use quiet text reporting
    
    * Setup projects
    
    * Remove PR template
    
    * Set name for releases
    
    * Fix Selene
    
    * Only run our tests
    
    * Be loud about it

--- .github/pull_request_template.md
+++
@@ -1,7 +0,0 @@
-# Problem
-
-Describe that problem you are trying to resolve
-
-# Solution
-
-Describe the solution you came up with
--- src/Components/App.lua
+++ src/Components/App.lua
@@ -1,20 +1,15 @@
 local Roact = require(script.Parent.Parent.Packages.Roact)
 local RoactHooks = require(script.Parent.Parent.Packages.RoactHooks)
-local t = require(script.Parent.Pa.Packages.t)
 
-local validateProps = t.interface({
-	storyRoot = t.instance,
-})
-
-local function App(props, hooks: any)
-	assert(validateProps(props))
-
-	local selectedStory, setSelectedStory = hooks.useState(nil)
-
+local function App(_props, _hooks: any)
 	return Roact.createElement("Frame", {
 		BackgroundTransparency = 1,
 		Size = UDim2.fromScale(1, 1),
-	}, {})
+	}, {
+		Label = Roact.createElement("TextLabel", {
+			Text = "Hello, World!",
+		}),
+	})
 end
 
 return RoactHooks.new(Roact)(App)
