8698bdb965c0046ae9a4a8e4613f6361da225f13
David Minnerly <me@vocksel.com>
2021-12-31 08:22:32 -0800

    Add types

---
+++ src/types.lua
@@ -0,0 +1,25 @@
+export type Storybook = {
+	storyRoots: { Instance },
+	name: string?,
+	summary: string?,
+	roact: table?,
+}
+
+export type StoryControl = {
+	name: string?,
+	type: string?,
+	value: any,
+}
+
+export type Story = {
+	name: string?,
+	summary: string?,
+	controls: { [string]: StoryControl },
+	story: table,
+}
+
+-- Legacy stories are functions that take a GuiObject to mount to and return
+-- another function which acts as the cleanup step.
+export type LegacyStory = (target: GuiObject) -> () -> nil
+
+return nil
