0d1d42854164c8e14dea04e10c661ec11c088374
vocksel <voxeldavid@gmail.com>
2021-12-31 15:16:39 -0500

    Cleanup (#2)
    * Use wrappers instead of props for sizing
    
    * Add types
    
    * Add StoryView
    
    * Fix Selene warning

--- src/Components/App.lua
+++ src/Components/App.lua
@@ -4,15 +4,39 @@ local Roact = require(script.Parent.Parent.Packages.Roact)
 local RoactHooks = require(script.Parent.Parent.Packages.RoactHooks)
 local getStories = require(script.Parent.Parent.getStories)
 local Sidebar = require(script.Parent.Sidebar)
+local StoryView = require(script.Parent.StoryView)
 
-local function App(_props, _hooks: any)
+local function App(_props, hooks: any)
+	local selectedStory, selectStory = hooks.useState(nil)
+
 	return Roact.createElement("Frame", {
 		BackgroundTransparency = 1,
 		Size = UDim2.fromScale(1, 1),
 	}, {
-		Sidebar = Roact.createElement(Sidebar, {
-			stories = getStories(ReplicatedStorage),
-			maxSize = Vector2.new(200, math.huge),
+		Layout = Roact.createElement("UIListLayout", {
+			SortOrder = Enum.SortOrder.LayoutOrder,
+			FillDirection = Enum.FillDirection.Horizontal,
+		}),
+
+		SidebarWrapper = Roact.createElement("Frame", {
+			LayoutOrder = 1,
+			Size = UDim2.fromScale(1 / 5, 1),
+			BackgroundTransparency = 1,
+		}, {
+			Sidebar = Roact.createElement(Sidebar, {
+				stories = getStories(ReplicatedStorage),
+				selectStory = selectStory,
+			}),
+		}),
+
+		StoryViewWrapper = Roact.createElement("Frame", {
+			LayoutOrder = 2,
+			Size = UDim2.fromScale(4 / 5, 1),
+			BackgroundTransparency = 1,
+		}, {
+			StoryView = Roact.createElement(StoryView, {
+				story = selectedStory,
+			}),
 		}),
 	})
 end
