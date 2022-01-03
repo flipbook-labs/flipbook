afb52f706c4435544e7df4f3e3fb845f8fb4b99d
David Minnerly <voxeldavid@gmail.com>
2022-01-02 17:29:20 -0800

    Add a name for the storybook

--- src/init.storybook.lua
+++ src/init.storybook.lua
@@ -1,6 +1,7 @@
 local Roact = require(script.Parent.Packages.Roact)
 
 return {
+	name = script.Parent.Name,
 	summary = "The one (and only) storybook for RoactStorybook",
 	storyRoots = {
 		script.Parent.Components,
