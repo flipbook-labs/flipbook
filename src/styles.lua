9cc9e4f550bff54aa66c821d31a1ce5c0cc23364
David Minnerly <voxeldavid@gmail.com>
2022-01-03 14:01:20 -0800

    Display storybooks and stories in sidebar

--- src/styles.lua
+++ src/styles.lua
@@ -1,5 +1,16 @@
 local styles = {}
 
+styles.TextLabel = {
+	TextSize = 16,
+	Font = Enum.Font.Gotham,
+	TextColor3 = Color3.fromRGB(255, 255, 255),
+	Size = UDim2.fromScale(0, 0),
+	AutomaticSize = Enum.AutomaticSize.XY,
+	TextXAlignment = Enum.TextXAlignment.Left,
+	TextYAlignment = Enum.TextYAlignment.Top,
+	BackgroundTransparency = 1,
+}
+
 styles.ScrollingFrame = {
 	Size = UDim2.fromScale(1, 1),
 	CanvasSize = UDim2.fromScale(1, 0),
