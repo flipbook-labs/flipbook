0ea680c702cd47adb253b54c3b8be64ce0800baf
vocksel <voxeldavid@gmail.com>
2022-01-07 19:36:21 -0500

    Create a better story list (#9)
    * Manage images through Tarmac
    
    * Add chevron image
    
    * Add WIP TreeList and TreeNode components
    
    * Finish TreeList
    
    * Only render the arrow if there are children
    
    * Hook up TreeList
    
    * Remove debug print
    
    * Use different image for stories
    
    * Hook up story selection
    
    * Fix Sidebar story

---
+++ src/Components/TreeList/types.lua
@@ -0,0 +1,8 @@
+export type Node = {
+	name: string,
+	icon: string?,
+	children: { Node },
+	instance: Instance?,
+}
+
+return {}
---
+++ src/Modules/createStoryNodes.lua
@@ -0,0 +1,47 @@
+local TreeList = require(script.Parent.Parent.Components.TreeList)
+local assets = require(script.Parent.Parent.assets)
+local constants = require(script.Parent.Parent.constants)
+local types = require(script.Parent.Parent.types)
+
+local function addStoriesToNode(root: Instance, node: TreeList.Node)
+	for _, child in ipairs(root:GetChildren()) do
+		local nextNode = {
+			name = child.Name,
+			instance = child,
+			children = {},
+		}
+
+		if child.Name:match(constants.STORY_NAME_PATTERN) then
+			nextNode.icon = assets.story
+			table.insert(node.children, nextNode)
+		else
+			if #child:GetChildren() > 0 then
+				nextNode.icon = assets.folder
+				table.insert(node.children, nextNode)
+				addStoriesToNode(child, nextNode)
+			end
+		end
+	end
+end
+
+local function createStoryNodes(storybooks: { types.Storybook }): { TreeList.Node }
+	local nodes = {}
+
+	for _, storybook in ipairs(storybooks) do
+		local node = {
+			name = storybook.name,
+			icon = assets.storybook,
+			children = {},
+		}
+
+		table.insert(nodes, node)
+
+		for _, root in ipairs(storybook.storyRoots) do
+			addStoriesToNode(root, node)
+		end
+	end
+
+	return nodes
+end
+
+return createStoryNodes
