29467fe28e7fb946a85a2cdd854f9294c17f1158
David Minnerly <voxeldavid@gmail.com>
2022-01-07 16:30:19 -0800

    Fix Sidebar story

--- src/Components/Sidebar.story.lua
+++ src/Components/Sidebar.story.lua
@@ -4,13 +4,9 @@ local Sidebar = require(script.Parent.Sidebar)
 return {
 	summary = "The sidebar that displays all the available stories for the current Storybook",
 	story = Roact.createElement(Sidebar, {
-		stories = {
-			script.Parent["Sample.story"],
-		},
 		storybooks = {
-			script.Parent.Parent["init.storybook"],
+			require(script.Parent.Parent["init.storybook"]),
 		},
-		onStorySelected = print,
-		onStorybookSelected = print,
+		selectStory = print,
 	}),
 }
