fe9e796207848763698873c984643b3cafe6b2b0
David Minnerly <voxeldavid@gmail.com>
2022-01-09 11:16:06 -0800

    Improve Hoarcekat story recognition

--- src/Formats/isHoarcekatStory.lua
+++ src/Formats/isHoarcekatStory.lua
@@ -1,12 +1,5 @@
 local function isHoarcekatStory(story: any)
-	if typeof(story) == "function" then
-		local _success, result = pcall(story)
-
-		if typeof(result) == "function" then
-			return true
-		end
-	end
-	return false
+	return typeof(story) == "function"
 end
 
 return isHoarcekatStory
