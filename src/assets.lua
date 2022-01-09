5c2d04195181dc92afce2e68238c9243f78bb90d
David Minnerly <voxeldavid@gmail.com>
2022-01-09 11:51:43 -0800

    Allow icons to be used with buttons

--- src/assets.lua
+++ src/assets.lua
@@ -3,7 +3,9 @@ return {
 	checkmark = "rbxassetid://8484527786",
 	["chevron-right"] = "rbxassetid://8472731876",
 	["double-arrow"] = "rbxassetid://8492343246",
+	explore = "rbxassetid://8500157719",
 	folder = "rbxassetid://8472668100",
 	story = "rbxassetid://8481250291",
 	storybook = "rbxassetid://8472668370",
+	viewport = "rbxassetid://8500158048",
 }
