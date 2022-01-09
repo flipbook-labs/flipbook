85547d8ebf90aaf9d8665a8236db3bc84cb00e98
David Minnerly <voxeldavid@gmail.com>
2022-01-09 14:30:03 -0800

    Revise Sidebar to include the toggle button

--- src/Components/App.lua
+++ src/Components/App.lua
@@ -2,30 +2,25 @@ local Roact = require(script.Parent.Parent.Packages.Roact)
 local RoactHooks = require(script.Parent.Parent.Packages.RoactHooks)
 local useStorybooks = require(script.Parent.Parent.Hooks.useStorybooks)
 local useTheme = require(script.Parent.Parent.Hooks.useTheme)
-local assets = require(script.Parent.Parent.assets)
 local Sidebar = require(script.Parent.Sidebar)
 local StoryView = require(script.Parent.StoryView)
 local NoStorySelected = require(script.Parent.NoStorySelected)
 
-local SIDEBAR_WIDTH_EXPANDED = 250 -- px
-local SIDEBAR_WIDTH_COLLAPSED = 24 -- px
+local SIDEBAR_WIDTH = NumberRange.new(24, 250)
 
 local function App(_props, hooks: any)
 	local theme = useTheme(hooks)
 	local storybooks = useStorybooks(hooks, game)
 	local story, selectStory = hooks.useState(nil)
-	local sidebarWidth, setSidebarWidth = hooks.useState(SIDEBAR_WIDTH_EXPANDED)
-	local isExpanded, setIsExpanded = hooks.useState(true)
+	local isSidebarExpanded, setIsSidebarExpanded = hooks.useState(true)
 
 	local toggleSidebar = hooks.useCallback(function()
-		setIsExpanded(function(prev)
+		setIsSidebarExpanded(function(prev)
 			return not prev
 		end)
-	end, { setIsExpanded })
+	end, { setIsSidebarExpanded })
 
-	hooks.useEffect(function()
-		setSidebarWidth(if isExpanded then SIDEBAR_WIDTH_EXPANDED else SIDEBAR_WIDTH_COLLAPSED)
-	end, { isExpanded })
+	local previewOffset = if isSidebarExpanded then -SIDEBAR_WIDTH.Max else 0
 
 	return Roact.createElement("Frame", {
 		BackgroundColor3 = theme:GetColor(Enum.StudioStyleGuideColor.MainBackground),
@@ -36,38 +31,18 @@ local function App(_props, hooks: any)
 			FillDirection = Enum.FillDirection.Horizontal,
 		}),
 
-		SidebarWrapper = Roact.createElement("Frame", {
-			LayoutOrder = 1,
-			Size = UDim2.new(0, sidebarWidth, 1, 0),
-			BackgroundTransparency = 1,
-		}, {
-			Toggle = Roact.createElement("ImageButton", {
-				Image = assets["double-arrow"],
-				Rotation = if isExpanded then 180 else 0,
-				ScaleType = Enum.ScaleType.Fit,
-				Size = UDim2.fromOffset(20, 28),
-				Position = UDim2.fromScale(1, 0.5),
-				AnchorPoint = Vector2.new(0.5, 0.5),
-				ZIndex = 2,
-				BackgroundColor3 = theme:GetColor(Enum.StudioStyleGuideColor.MainBackground),
-				[Roact.Event.Activated] = toggleSidebar,
-			}, {
-				Border = Roact.createElement("UIStroke", {
-					Color = theme:GetColor(Enum.StudioStyleGuideColor.Border),
-					ApplyStrokeMode = Enum.ApplyStrokeMode.Border,
-					Thickness = 2,
-				}),
-			}),
-
-			Sidebar = Roact.createElement(Sidebar, {
-				storybooks = storybooks,
-				selectStory = selectStory,
-			}),
+		Sidebar = Roact.createElement(Sidebar, {
+			layoutOrder = 1,
+			storybooks = storybooks,
+			selectStory = selectStory,
+			isExpanded = isSidebarExpanded,
+			width = SIDEBAR_WIDTH,
+			onToggleActivated = toggleSidebar,
 		}),
 
 		StoryViewWrapper = Roact.createElement("Frame", {
 			LayoutOrder = 2,
-			Size = UDim2.new(1, -sidebarWidth, 1, 0),
+			Size = UDim2.new(1, previewOffset, 1, 0),
 			BackgroundTransparency = 1,
 		}, {
 			StoryView = story and Roact.createElement(StoryView, {
--- src/Components/Sidebar.lua
+++ src/Components/Sidebar.lua
@@ -8,14 +8,20 @@ local styles = require(script.Parent.Parent.styles)
 local types = require(script.Parent.Parent.types)
 local Panel = require(script.Parent.Panel)
 local TreeList = require(script.Parent.TreeList)
+local SidebarToggle = require(script.Parent.SidebarToggle)
 
 type Props = {
+	isExpanded: boolean,
+	width: NumberRange,
 	storybooks: { types.Storybook },
 	selectStory: (types.Story) -> nil,
+	layoutOrder: number?,
+	onToggleActivated: (() -> nil)?,
 }
 
 local function Sidebar(props: Props, hooks: any)
 	local theme = useTheme(hooks)
+	local width = if props.isExpanded then props.width.Max else props.width.Min
 
 	local onNodeActivated = hooks.useCallback(function(node: TreeList.Node)
 		if node.instance and node.name:match(constants.STORY_NAME_PATTERN) then
@@ -34,16 +40,29 @@ local function Sidebar(props: Props, hooks: any)
 		nodes = storybookNodes,
 	})
 
-	return Roact.createElement(Panel, {}, {
-		Layout = Roact.createElement("UIListLayout"),
+	return Roact.createElement("Frame", {
+		LayoutOrder = props.layoutOrder,
+		Size = UDim2.new(0, width, 1, 0),
+		BackgroundTransparency = 1,
+	}, {
+		Toggle = Roact.createElement(SidebarToggle, {
+			isExpanded = props.isExpanded,
+			onActivated = props.onToggleActivated,
+			position = UDim2.fromScale(1, 0.5),
+			anchorPoint = Vector2.new(0.5, 0.5),
+		}),
 
-		ScrollingFrame = Roact.createElement(
-			"ScrollingFrame",
-			Llama.Dictionary.join(styles.ScrollingFrame, {
-				BackgroundColor3 = theme:GetColor(Enum.StudioStyleGuideColor.MainBackground),
-			}),
-			children
-		),
+		Panel = Roact.createElement(Panel, {}, {
+			Layout = Roact.createElement("UIListLayout"),
+
+			ScrollingFrame = Roact.createElement(
+				"ScrollingFrame",
+				Llama.Dictionary.join(styles.ScrollingFrame, {
+					BackgroundColor3 = theme:GetColor(Enum.StudioStyleGuideColor.MainBackground),
+				}),
+				children
+			),
+		}),
 	})
 end
 
