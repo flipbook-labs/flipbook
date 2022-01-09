a0f9a4b742cd899e212a204c03772d8541a11f04
David Minnerly <voxeldavid@gmail.com>
2022-01-09 10:57:06 -0800

    Fix failing test

--- src/Modules/isStorybookModule.lua
+++ src/Modules/isStorybookModule.lua
@@ -1,7 +1,7 @@
 local constants = require(script.Parent.Parent.constants)
 
 local function isStorybookModule(instance: Instance): boolean
-	return instance:IsA("ModuleScript") and instance.Name:match(constants.STORYBOOK_NAME_PATTERN)
+	return instance:IsA("ModuleScript") and instance.Name:match(constants.STORYBOOK_NAME_PATTERN) ~= nil
 end
 
 return isStorybookModule
