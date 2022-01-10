4edc823f7dcd23c8f8736c20073c028e61b4d58f
vocksel <voxeldavid@gmail.com>
2022-01-10 13:59:12 -0800

    Apply padding within the story preview (#40)

--- src/Components/SidebarToggle.story.lua
+++ src/Components/SidebarToggle.story.lua
@@ -1,6 +1,5 @@
 local Roact = require(script.Parent.Parent.Packages.Roact)
 local RoactHooks = require(script.Parent.Parent.Packages.RoactHooks)
-local styles = require(script.Parent.Parent.styles)
 local SidebarToggle = require(script.Parent.SidebarToggle)
 
 local function Story(_props, hooks: any)
@@ -12,18 +11,9 @@ local function Story(_props, hooks: any)
 		end)
 	end, {})
 
-	return Roact.createFragment({
-		Padding = Roact.createElement("UIPadding", {
-			PaddingTop = styles.LARGE_PADDING,
-			PaddingRight = styles.LARGE_PADDING,
-			PaddingBottom = styles.LARGE_PADDING,
-			PaddingLeft = styles.LARGE_PADDING,
-		}),
-
-		Story = Roact.createElement(SidebarToggle, {
-			isExpanded = isExpanded,
-			onActivated = toggle,
-		}),
+	return Roact.createElement(SidebarToggle, {
+		isExpanded = isExpanded,
+		onActivated = toggle,
 	})
 end
 
