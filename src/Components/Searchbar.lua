260950bf651b38d169385ccd0f44c73203e7f333
pashleyy <64950564+passhley@users.noreply.github.com>
2022-01-13 19:18:26 -0500

    Added padding

--- src/Components/Searchbar.lua
+++ src/Components/Searchbar.lua
@@ -3,6 +3,9 @@ local Icon = require(script.Parent.Icon)
 local Roact = require(script.Parent.Parent.Packages.Roact)
 local useTheme = require(script.Parent.Parent.Hooks.useTheme)
 
+local PADDING = UDim.new(0, 9)
+local NO_PADDING = UDim.new(0, 0)
+
 type Props = {
 	position: UDim2?,
 }
@@ -15,6 +18,13 @@ local function Searchbar(props: Props, hooks: any)
 		Position = props.position,
 		Size = UDim2.fromOffset(200, 32),
 	}, {
+		UIPadding = Roact.createElement("UIPadding", {
+			PaddingLeft = PADDING,
+			PaddingRight = PADDING,
+			PaddingTop = NO_PADDING,
+			PaddingBottom = NO_PADDING,
+		}),
+
 		UICorner = Roact.createElement("UICorner", {
 			CornerRadius = UDim.new(0, 4),
 		}),
@@ -24,9 +34,19 @@ local function Searchbar(props: Props, hooks: any)
 		}),
 
 		Icon = Roact.createElement(Icon, {
+			anchorPoint = Vector2.new(0, 0.5),
 			color = theme.icons.search,
 			icon = "magnifying-glass",
+			position = UDim2.fromScale(0, 0.5),
 			size = 14,
+		}),
+
+		Divider = Roact.createElement("Frame", {
+			AnchorPoint = Vector2.new(0, 0.5),
+			BackgroundColor3 = theme.stroke,
+			BorderSizePixel = 0,
+			Position = UDim2.new(0, 23, 0.5, 0),
+			Size = UDim2.new(0, 1, 1, -8),
 		}),
 	})
 end
