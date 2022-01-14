823dc0c73133cb248a512c508653bf344e22d7ca
pashleyy <64950564+passhley@users.noreply.github.com>
2022-01-13 19:39:26 -0500

    Adding active states

--- src/Components/Searchbar.lua
+++ src/Components/Searchbar.lua
@@ -1,20 +1,43 @@
+local Flipper = require(script.Parent.Parent.Packages.Flipper)
 local hook = require(script.Parent.Parent.hook)
 local Icon = require(script.Parent.Icon)
 local Roact = require(script.Parent.Parent.Packages.Roact)
+local useSingleMotor = require(script.Parent.Parent.Hooks.useSingleMotor)
 local useTheme = require(script.Parent.Parent.Hooks.useTheme)
 
 local PADDING = UDim.new(0, 9)
 local NO_PADDING = UDim.new(0, 0)
+local SPRING_OPTIONS = {
+	dampingRatio = 0.7,
+	frequency = 6,
+}
 
 type Props = {
 	position: UDim2?,
 }
 
+local function mapColors(spring, color1, color2)
+	return spring:map(function(alpha)
+		return color1:Lerp(color2, alpha)
+	end)
+end
+
 local function Searchbar(props: Props, hooks: any)
 	local theme = useTheme(hooks)
+	local spring, setSpring = useSingleMotor(hooks, 0)
+	local active, setActive = hooks.useState(false)
+
+	hooks.useEffect(function()
+		if active then
+			setSpring(Flipper.Spring.new(0, SPRING_OPTIONS))
+		else
+			setSpring(Flipper.Spring.new(1))
+		end
+	end, { active, setSpring })
 
 	return Roact.createElement("Frame", {
-		BackgroundTransparency = 1,
+		BackgroundColor3 = theme.searchbar.background,
+		BackgroundTransparency = spring,
 		Position = props.position,
 		Size = UDim2.fromOffset(200, 32),
 	}, {
@@ -30,12 +53,12 @@ local function Searchbar(props: Props, hooks: any)
 		}),
 
 		UIStroke = Roact.createElement("UIStroke", {
-			Color = theme.stroke,
+			Color = mapColors(spring, theme.searchbar.stroke, theme.stroke),
 		}),
 
 		Icon = Roact.createElement(Icon, {
 			anchorPoint = Vector2.new(0, 0.5),
-			color = theme.stroke,
+			color = mapColors(spring, theme.icons.search, theme.stroke),
 			icon = "magnifying-glass",
 			position = UDim2.fromScale(0, 0.5),
 			size = 14,
@@ -43,7 +66,7 @@ local function Searchbar(props: Props, hooks: any)
 
 		Divider = Roact.createElement("Frame", {
 			AnchorPoint = Vector2.new(0, 0.5),
-			BackgroundColor3 = theme.stroke,
+			BackgroundColor3 = mapColors(spring, theme.searchbar.stroke, theme.stroke),
 			BorderSizePixel = 0,
 			Position = UDim2.new(0, 23, 0.5, 0),
 			Size = UDim2.new(0, 1, 1, -8),
@@ -57,6 +80,19 @@ local function Searchbar(props: Props, hooks: any)
 			Text = "Find components",
 			TextColor3 = theme.stroke,
 			TextSize = 10,
+		}),
+
+		Hitbox = Roact.createElement("TextButton", {
+			BackgroundTransparency = 1,
+			Size = UDim2.fromScale(1, 1),
+			Text = "",
+			[Roact.Event.Activated] = function()
+				if active then
+					setActive(false)
+				else
+					setActive(true)
+				end
+			end,
 		}),
 	})
 end
