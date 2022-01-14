7be8f4513e48571635bb31288c085560060a8658
pashleyy <64950564+passhley@users.noreply.github.com>
2022-01-13 19:39:20 -0500

    Added flipper hooks

---
+++ src/Hooks/useSingleMotor.lua
@@ -0,0 +1,18 @@
+local Flipper = require(script.Parent.Parent.Packages.Flipper)
+
+local function useSingleMotor(hooks, initialValue)
+	local motor = hooks.useValue(Flipper.SingleMotor.new(initialValue)).value
+	local binding, setBinding = hooks.useBinding(motor:getValue())
+
+	hooks.useEffect(function()
+		motor:onStep(setBinding)
+	end)
+
+	local function setGoal(goal)
+		motor:setGoal(goal)
+	end
+
+	return binding, setGoal, motor
+end
+
+return useSingleMotor
