e0ef299f086fa5e16088d2b96ad4d52a365e34ea
David Minnerly <voxeldavid@gmail.com>
2022-01-19 17:25:52 -0800

    Fix useTheme throwing errors in local tests

--- src/Hooks/useTheme.lua
+++ src/Hooks/useTheme.lua
@@ -1,7 +1,20 @@
+local MOCK_STUDIO = {
+	ThemeChanged = Instance.new("BindableEvent").Event,
+	Theme = {
+		GetColor = function() end,
+	},
+}
+
 local function useTheme(hooks: any)
 	local studio = hooks.useMemo(function()
-		return settings().Studio
+		local success, result = pcall(function()
+			return settings().Studio
+		end)
+
+		return if success then result else MOCK_STUDIO
 	end, {})
+
+	print(studio)
 
 	local theme: StudioTheme, set = hooks.useState(studio.Theme)
 
