2389f26dac4fc7ea44c058725b3a57ed25e96132
David Minnerly <voxeldavid@gmail.com>
2022-05-10 22:07:15 -0700

    Fix packages not being ignored

---
+++ bin/analyze.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+curl -s -O https://raw.githubusercontent.com/JohnnyMorganz/luau-analyze-rojo/master/globalTypes.d.lua
+cp .github/workflows/.luaurc Packages
+
+luau-analyze --project=dev.project.json --defs=globalTypes.d.lua --defs=testez.d.lua src/
+
+rm Packages/.luaurc
+rm globalTypes.d.lua
