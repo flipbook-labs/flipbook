71c18b20cfe8dee0dc49469ac27b8f7087aa1eca
David Minnerly <voxeldavid@gmail.com>
2022-06-04 12:13:58 -0700

    Add stubs for tests

--- src/Modules/createStoryNodes.lua
+++ src/Modules/createStoryNodes.lua
@@ -1,18 +1,19 @@
 local Explorer = require(script.Parent.Parent.Components.Explorer)
-local constants = require(script.Parent.Parent.constants)
 local types = require(script.Parent.Parent.types)
+local isStoryModule = require(script.Parent.isStoryModule)
 
-local function hasStoriesInRoot(root: Instance): boolean
-	for _, child in ipairs(root:GetChildren()) do
-		if child.Name:match(constants.STORY_NAME_PATTERN) then
+local function hasStories(instance: Instance): boolean
+	for _, descendant in ipairs(instance:GetDescendants()) do
+		if isStoryModule(descendant) then
 			return true
 		end
 	end
-
 	return false
 end
 
 local function addStoriesToNode(root: Instance, node: Explorer.Node, storybook: types.Storybook)
+	-- TODO: Maybe use GetDescendants instead and for every .story file, use
+	-- GetFullName() and go up the chain the build the hierarchy
 	for _, child in ipairs(root:GetChildren()) do
 		local nextNode = {
 			name = child.Name,
@@ -20,13 +21,15 @@ local function addStoriesToNode(root: Instance, node: Explorer.Node, storybook: 
 			children = {},
 		}
 
-		if child.Name:match(constants.STORY_NAME_PATTERN) then
+		if isStoryModule(child) then
 			nextNode.icon = "story"
 			nextNode.storybook = storybook
 			table.insert(node.children, nextNode)
 		else
 			if #child:GetChildren() > 0 then
-				if hasStoriesInRoot(child) then
+				-- This is why my stories arent showing up. The folders have no
+				-- stories as direct descendants so of course they don't show up
+				if hasStories(child) then
 					nextNode.icon = "folder"
 					table.insert(node.children, nextNode)
 					addStoriesToNode(child, nextNode, storybook)
@@ -35,6 +38,12 @@ local function addStoriesToNode(root: Instance, node: Explorer.Node, storybook: 
 		end
 	end
 end
+
+-- Got an idea for how to construct this: Clone each storyRoot,  loop over the
+-- list of descendants for each, and remove any that aren't stories or part of a
+-- story's ancestry
+--
+-- Can then map that right to the story nodes
 
 local function createStoryNodes(storybooks: { types.Storybook }): { Explorer.Node }
 	local nodes = {}
---
+++ src/Modules/createStoryNodes.spec.lua
@@ -0,0 +1,12 @@
+return function()
+	local Roact = require(script.Parent.Parent.Packages.Roact)
+	local createStoryNodes = require(script.Parent.createStoryNodes)
+
+	itFIXME("should use an icon for storybooks", function() end)
+
+	itFIXME("should use an icon for stories", function() end)
+
+	itFIXME("should use an icon for container instances", function() end)
+
+	itFIXME("should return a Node hierarchy for stories", function() end)
+end
