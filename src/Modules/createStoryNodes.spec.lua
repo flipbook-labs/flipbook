72453d48fbd11bbc5ecbb37affd0612c9e8519d8
David Minnerly <voxeldavid@gmail.com>
2022-06-05 15:19:29 -0700

    Add tests for createStoryNodes

--- src/Modules/createStoryNodes.spec.lua
+++ src/Modules/createStoryNodes.spec.lua
@@ -1,12 +1,48 @@
 return function()
-	local Roact = require(script.Parent.Parent.Packages.Roact)
+	local flipbook = script:FindFirstAncestor("flipbook")
+	local newFolder = require(flipbook.Mocks.newFolder)
+	local types = require(script.Parent.Parent.types)
 	local createStoryNodes = require(script.Parent.createStoryNodes)
 
-	itFIXME("should use an icon for storybooks", function() end)
+	local mockStoryModule = Instance.new("ModuleScript")
 
-	itFIXME("should use an icon for stories", function() end)
+	local mockStoryRoot = newFolder({
+		Components = newFolder({
+			["Story.story"] = mockStoryModule,
+		}),
+	})
 
-	itFIXME("should use an icon for container instances", function() end)
+	local mockStorybook: types.Storybook = {
+		name = "MockStorybook",
+		storyRoots = { mockStoryRoot },
+	}
 
-	itFIXME("should return a Node hierarchy for stories", function() end)
+	it("should use an icon for storybooks", function()
+		local nodes = createStoryNodes({ mockStorybook })
+
+		local storybook = nodes[1]
+		expect(storybook).to.be.ok()
+		expect(storybook.icon).to.equal("storybook")
+	end)
+
+	it("should use an icon for container instances", function()
+		local nodes = createStoryNodes({ mockStorybook })
+
+		local storybook = nodes[1]
+		local components = storybook.children[1]
+
+		expect(components).to.be.ok()
+		expect(components.icon).to.equal("folder")
+	end)
+
+	it("should use an icon for stories", function()
+		local nodes = createStoryNodes({ mockStorybook })
+
+		local storybook = nodes[1]
+		local components = storybook.children[1]
+		local story = components.children[1]
+
+		expect(story).to.be.ok()
+		expect(story.icon).to.equal("story")
+	end)
 end
