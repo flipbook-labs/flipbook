4436184db18ac3e1aaa72b51419788fa2899d012
David Minnerly <voxeldavid@gmail.com>
2022-07-01 12:50:03 -0700

    Use luau-lsp for analysis

--- bin/analyze.sh
+++ bin/analyze.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-curl -s -O https://raw.githubusercontent.com/JohnnyMorganz/luau-analyze-rojo/master/globalTypes.d.lua
+curl -s -O https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/master/scripts/globalTypes.d.lua
 
-luau-analyze --project=dev.project.json --defs=globalTypes.d.lua --defs=testez.d.lua src/
+rojo sourcemap dev.project.json -o sourcemap.json
+luau-lsp analyze --sourcemap=sourcemap.json --defs=globalTypes.d.lua --defs=testez.d.lua src/
 
-rm Packages/.luaurc
 rm globalTypes.d.lua
