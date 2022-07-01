940e1be0dc4d9b0a01eddca5b3fc2d71acd9a75b
David Minnerly <voxeldavid@gmail.com>
2022-07-01 15:12:37 -0700

    Fix packages being analyzed
    Turns out `wally install` nukes the entire directory contents, so the .luaurc file was getting deleted

--- bin/analyze.sh
+++ bin/analyze.sh
@@ -2,7 +2,10 @@
 
 curl -s -O https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/master/scripts/globalTypes.d.lua
 
+cp .github/workflows/.luaurc Packages
 rojo sourcemap dev.project.json -o sourcemap.json
+
 luau-lsp analyze --sourcemap=sourcemap.json --defs=globalTypes.d.lua --defs=testez.d.lua src/
 
+rm Packages/.luaurc
 rm globalTypes.d.lua
