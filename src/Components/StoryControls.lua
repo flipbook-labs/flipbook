239eb914a78fc18391fb2aa86bbe1ae72053bfc2
Marin Minnerly <me@vocksel.com>
2022-07-01 16:15:52 -0700

    Merge StoryControlsNavbar into StoryControls

--- src/Components/StoryControls.lua
+++ src/Components/StoryControls.lua
@@ -2,7 +2,8 @@ local flipbook = script:FindFirstAncestor("flipbook")
 
 local Roact = require(flipbook.Packages.Roact)
 local hook = require(flipbook.hook)
-local StoryControlsNavbar = require(flipbook.Components.StoryControlsNavbar)
+local Navbar = require(flipbook.Components.Navbar)
+local useTailwind = require(flipbook.Hooks.useTailwind)
 
 local e = Roact.createElement
 
@@ -16,8 +17,58 @@ local function StoryControls(props: Props)
 		LayoutOrder = props.layoutOrder,
 		Size = UDim2.fromScale(1, 0),
 	}, {
-		StoryControlsNavbar = e(StoryControlsNavbar, {
-			layoutOrder = 1,
+		Navbar = e(Navbar.Element, {
+			height = 55,
+			layoutOrder = props.layoutOrder,
+			topDivider = true,
+		}, {
+			Content = e(Navbar.Items, {
+				layoutOrder = 1,
+				padding = UDim.new(0, 12),
+			}, {
+				Pages = e(Navbar.Items, {
+					layoutOrder = 1,
+					padding = UDim.new(0, 0),
+				}, {
+					Controls = e(Navbar.Item, {
+						active = true,
+						layoutOrder = 1,
+						onClick = function() end,
+						padding = { x = UDim.new(0, 10), y = UDim.new(0, 10) },
+					}, {
+						Text = e("TextLabel", {
+							AutomaticSize = Enum.AutomaticSize.XY,
+							BackgroundTransparency = 1,
+							Font = Enum.Font.GothamMedium,
+							Size = UDim2.fromScale(0, 0),
+							Text = "Controls",
+							TextColor3 = useTailwind("gray-800"),
+							TextSize = 14,
+							TextXAlignment = Enum.TextXAlignment.Left,
+							TextYAlignment = Enum.TextYAlignment.Top,
+						}),
+					}),
+
+					Story = e(Navbar.Item, {
+						active = false,
+						layoutOrder = 2,
+						onClick = function() end,
+						padding = { x = UDim.new(0, 10), y = UDim.new(0, 10) },
+					}, {
+						Text = e("TextLabel", {
+							AutomaticSize = Enum.AutomaticSize.XY,
+							BackgroundTransparency = 1,
+							Font = Enum.Font.GothamMedium,
+							Size = UDim2.fromScale(0, 0),
+							Text = "Story",
+							TextColor3 = useTailwind("gray-600"),
+							TextSize = 14,
+							TextXAlignment = Enum.TextXAlignment.Left,
+							TextYAlignment = Enum.TextYAlignment.Top,
+						}),
+					}),
+				}),
+			}),
 		}),
 	})
 end
