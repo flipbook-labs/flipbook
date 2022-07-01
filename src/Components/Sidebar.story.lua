5523e309e6da7c36dad925860cff149cc8f5bae4
Marin Minnerly <me@vocksel.com>
2022-07-01 16:21:25 -0700

    Fix Sidebar story

--- src/Components/Sidebar.story.lua
+++ src/Components/Sidebar.story.lua
@@ -1,10 +1,17 @@
 local flipbook = script:FindFirstAncestor("flipbook")
 
 local Roact = require(flipbook.Packages.Roact)
+local internalStorybook = require(flipbook["init.storybook"])
 local Sidebar = require(script.Parent.Sidebar)
 
 return {
 	summary = "Sidebar containing brand, searchbar, and component tree",
 	controls = {},
-	story = Roact.createElement(Sidebar),
+	story = Roact.createElement(Sidebar, {
+		storybooks = {
+			internalStorybook,
+		},
+		selectStory = print,
+		selectStorybook = print,
+	}),
 }
