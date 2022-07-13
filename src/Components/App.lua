30bf28d8cbe390ac91420601eb0c9ffb89b25fd1
vocksel <me@vocksel.com>
2022-07-13 00:18:54 -0400

    Add functionality for "View Code" button (#114)
    Have "View Code" button open the story

--- src/Components/App.lua
+++ src/Components/App.lua
@@ -5,12 +5,17 @@ local Roact = require(flipbook.Packages.Roact)
 local hook = require(flipbook.hook)
 local useStorybooks = require(flipbook.Hooks.useStorybooks)
 local useTheme = require(flipbook.Hooks.useTheme)
+local PluginContext = require(flipbook.Plugin.PluginContext)
 local Canvas = require(script.Parent.Canvas)
 local Sidebar = require(script.Parent.Sidebar)
 
 local loader = ModuleLoader.new()
 
-local function App(_props, hooks: any)
+export type Props = {
+	plugin: Plugin,
+}
+
+local function App(props: Props, hooks: any)
 	local theme = useTheme(hooks)
 	local storybooks = useStorybooks(hooks, game, loader)
 	local story, setStory = hooks.useState(nil)
@@ -22,28 +27,32 @@ local function App(_props, hooks: any)
 		end)
 	end, { setStory })
 
-	return Roact.createElement("Frame", {
-		BackgroundColor3 = theme.background,
-		Size = UDim2.fromScale(1, 1),
+	return Roact.createElement(PluginContext.Provider, {
+		plugin = props.plugin,
 	}, {
-		UIListLayout = Roact.createElement("UIListLayout", {
-			FillDirection = Enum.FillDirection.Horizontal,
-			SortOrder = Enum.SortOrder.LayoutOrder,
-			VerticalAlignment = Enum.VerticalAlignment.Center,
-		}),
+		Roact.createElement("Frame", {
+			BackgroundColor3 = theme.background,
+			Size = UDim2.fromScale(1, 1),
+		}, {
+			UIListLayout = Roact.createElement("UIListLayout", {
+				FillDirection = Enum.FillDirection.Horizontal,
+				SortOrder = Enum.SortOrder.LayoutOrder,
+				VerticalAlignment = Enum.VerticalAlignment.Center,
+			}),
 
-		Sidebar = Roact.createElement(Sidebar, {
-			layoutOrder = 1,
-			selectStory = selectStory,
-			selectStorybook = selectStorybook,
-			storybooks = storybooks,
-		}),
+			Sidebar = Roact.createElement(Sidebar, {
+				layoutOrder = 1,
+				selectStory = selectStory,
+				selectStorybook = selectStorybook,
+				storybooks = storybooks,
+			}),
 
-		Canvas = Roact.createElement(Canvas, {
-			layoutOrder = 2,
-			loader = loader,
-			story = story,
-			storybook = storybook,
+			Canvas = Roact.createElement(Canvas, {
+				layoutOrder = 2,
+				loader = loader,
+				story = story,
+				storybook = storybook,
+			}),
 		}),
 	})
 end
