404a59cf064060fc41f65e7ce86d3b10ca9f289d
vocksel <me@vocksel.com>
2022-07-28 19:48:04 -0700

    Fix Studio crashing when editing a story (#130)
    Fixx Studio crashing when editing storybooks

--- src/Hooks/useStory.lua
+++ src/Hooks/useStory.lua
@@ -20,7 +20,7 @@ local function useStory(hooks: any, module: ModuleScript, storybook: types.Story
 			story = story,
 			err = err,
 		})
-	end, { module })
+	end, { loader, module, storybook })
 
 	hooks.useEffect(function()
 		local conn = loader.loadedModuleChanged:Connect(loadStory)
@@ -30,7 +30,7 @@ local function useStory(hooks: any, module: ModuleScript, storybook: types.Story
 		return function()
 			conn:Disconnect()
 		end
-	end, { module })
+	end, { module, loadStory, loader })
 
 	return state.story, state.err
 end
