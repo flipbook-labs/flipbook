a6be8e6264c7585f05ebafa6f4cf34b78dd3d2c6
Marin Minnerly <me@vocksel.com>
2022-12-18 16:36:16 -0800

    Add some placeholder elements for controls

--- src/Components/StoryControls.lua
+++ src/Components/StoryControls.lua
@@ -4,15 +4,40 @@ local Roact = require(flipbook.Packages.Roact)
 local hook = require(flipbook.hook)
 local Navbar = require(flipbook.Components.Navbar)
 local useTheme = require(flipbook.Hooks.useTheme)
+local types = require(flipbook.types)
 
 local e = Roact.createElement
 
 type Props = {
 	layoutOrder: number,
+	controls: { types.StoryControl },
 }
 
 local function StoryControls(props: Props, hooks: any)
 	local theme = useTheme(hooks)
+
+	local controls = {}
+	controls.Layout = e("UIListLayout", {
+		SortOrder = Enum.SortOrder.LayoutOrder,
+		Padding = theme.padding,
+	})
+	for index, control in props.controls do
+		controls[control.name] = e("Frame", {}, {
+			Name = e("TextLabel", {
+				LayoutOrder = index,
+				Text = control.name,
+				AutomaticSize = Enum.AutomaticSize.XY,
+				BackgroundTransparency = 1,
+				Font = theme.font,
+				TextColor3 = theme.text,
+				TextSize = theme.headerTextSize,
+				TextXAlignment = Enum.TextXAlignment.Left,
+				TextYAlignment = Enum.TextYAlignment.Top,
+			}),
+
+			Option = e("TextLabel"),
+		})
+	end
 
 	return e("Frame", {
 		BackgroundTransparency = 1,
@@ -26,15 +51,21 @@ local function StoryControls(props: Props, hooks: any)
 		}, {
 			Content = e(Navbar.Items, {
 				layoutOrder = 1,
-				padding = UDim.new(0, 12),
+				padding = theme.padding,
 			}, {
 				Controls = e(Navbar.Item, {
 					active = true,
 					layoutOrder = 1,
 					onClick = function() end,
-					padding = { x = UDim.new(0, 10), y = UDim.new(0, 10) },
+					padding = { x = theme.padding, y = theme.padding },
 				}, {
-					Text = e("TextLabel", {
+					Layout = e("UIListLayout", {
+						SortOrder = Enum.SortOrder.LayoutOrder,
+						Padding = theme.padding,
+					}),
+
+					Title = e("TextLabel", {
+						LayoutOrder = 1,
 						AutomaticSize = Enum.AutomaticSize.XY,
 						BackgroundTransparency = 1,
 						Font = theme.headerFont,
@@ -45,6 +76,13 @@ local function StoryControls(props: Props, hooks: any)
 						TextXAlignment = Enum.TextXAlignment.Left,
 						TextYAlignment = Enum.TextYAlignment.Top,
 					}),
+
+					Controls = e("Frame", {
+						LayoutOrder = 2,
+						Size = UDim2.fromScale(1, 0),
+						AutomaticSize = Enum.AutomaticSize.Y,
+						BackgroundTransparency = 1,
+					}, controls),
 				}),
 			}),
 		}),
--- src/Components/StoryControls.story.lua
+++ src/Components/StoryControls.story.lua
@@ -5,5 +5,24 @@ local StoryControls = require(script.Parent.StoryControls)
 
 return {
 	summary = "Panel for configuring the controls of a story",
-	story = Roact.createElement(StoryControls, {}),
+	story = Roact.createElement(StoryControls, {
+		controls = {
+			{
+				name = "Foo",
+				value = "bar",
+			},
+			{
+				name = "Checkbox",
+				value = false,
+			},
+			{
+				name = "Dropdown Selection",
+				value = {
+					"Option 1",
+					"Option 2",
+					"Option 3",
+				},
+			},
+		},
+	}),
 }
