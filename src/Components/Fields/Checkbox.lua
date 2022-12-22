053cdb765c4aa73265ce93a8e3dd83456c2ce505
David Minnerly <me@vocksel.com>
2022-12-22 10:47:14 -0800

    Fix checkbox controls

--- src/Components/Fields/Checkbox.lua
+++ src/Components/Fields/Checkbox.lua
@@ -13,12 +13,20 @@ local function Checkbox(props: Props, hooks: any)
 	local theme = useTheme(hooks)
 	local isChecked, setIsChecked = hooks.useState(props.initialState)
 
+	local toggle = hooks.useCallback(function()
+		local newValue = not isChecked
+
+		if props.onStateChange then
+			props.onStateChange(newValue)
+		end
+
+		setIsChecked(newValue)
+	end, { isChecked })
+
 	return Roact.createElement("ImageButton", {
 		BackgroundColor3 = theme.button,
 		Size = UDim2.fromOffset(theme.textSize, theme.textSize) + UDim2.new(theme.padding, theme.padding),
-		[Roact.Event.Activated] = function()
-			setIsChecked(not isChecked)
-		end,
+		[Roact.Event.Activated] = toggle,
 	}, {
 		Layout = Roact.createElement("UIListLayout", {
 			HorizontalAlignment = Enum.HorizontalAlignment.Center,
