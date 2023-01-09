13ad1d83b8afa084692f24f1fd2896253b94abd2
David Minnerly <me@vocksel.com>
2023-01-08 17:02:59 -0800

    Remove function call that breaks story tests

--- src/stories.spec.lua
+++ src/stories.spec.lua
@@ -2,16 +2,10 @@ local CoreGui = game:GetService("CoreGui")
 
 local flipbook = script:FindFirstAncestor("flipbook")
 
-local React = require(script.Parent.Packages.React)
 local isStoryModule = require(script.Parent.Story.isStoryModule)
 local mountStory = require(script.Parent.Story.mountStory)
 
 return function()
-	React.setGlobalConfig({
-		propValidation = true,
-		elementTracing = true,
-	})
-
 	for _, descendant in ipairs(flipbook.Components:GetDescendants()) do
 		if isStoryModule(descendant) then
 			it("should mount/unmount " .. descendant.Name, function()
