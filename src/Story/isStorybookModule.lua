c9b4b78c0c1a5dfa7ecb355855f45e04343381f0
Marin Minnerly <me@vocksel.com>
2023-08-05 12:37:04 -0700

    Filter out storybooks in CoreGui

--- src/Story/isStorybookModule.lua
+++ src/Story/isStorybookModule.lua
@@ -1,9 +1,13 @@
 local flipbook = script:FindFirstAncestor("flipbook")
 
+local CoreGui = game:GetService("CoreGui")
+
 local constants = require(flipbook.constants)
 
 local function isStorybookModule(instance: Instance): boolean
-	return instance:IsA("ModuleScript") and instance.Name:match(constants.STORYBOOK_NAME_PATTERN) ~= nil
+	return instance:IsA("ModuleScript")
+		and instance.Name:match(constants.STORYBOOK_NAME_PATTERN) ~= nil
+		and not instance:IsDescendantOf(CoreGui)
 end
 
 return isStorybookModule
