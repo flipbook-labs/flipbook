711de2f1efdc661a1b4adc83e4dc6a893629070d
Marin Minnerly <me@vocksel.com>
2023-11-24 16:44:52 -0800

    Use tests.project.json for analysis

--- bin/analyze.sh
+++ bin/analyze.sh
@@ -2,7 +2,7 @@
 
 curl -s -O https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/master/scripts/globalTypes.d.lua
 
-rojo sourcemap dev.project.json -o sourcemap.json
+rojo sourcemap tests.project.json -o sourcemap.json
 
 luau-lsp analyze --sourcemap=sourcemap.json --defs=globalTypes.d.lua --defs=testez.d.lua --ignore=**/_Index/** src/
 
