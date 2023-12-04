f72c57ad9bfaf3db68791e2e0e04c5e3f6c4bfbd
hy2k <17329720+hy2k@users.noreply.github.com>
2023-12-04 11:11:14 +0900

    Fix Searchbar filtering (#231)
    # Problem
    
    The current implementation of the filtering functionality in our
    Storybook tree view has a limitation. It only checks if the name of the
    current component node matches the filter text. This means that even if
    a story matches the filter, if any of its parent folders does not match
    the filter, the entire subtree gets skipped during rendering.
    
    # Solution
    
    The proposed solution modifies this behavior for non-story nodes (e.g.,
    storybooks, folders). Now, instead of checking if the node's name
    matches the filter, we check if the node has at least one descendant
    story that matches the filter. This ensures that folders containing
    matching stories are not erroneously hidden due to the folder name not
    matching the filter.
    
    For story nodes, the behavior remains the same as before: the story is
    displayed if its name matches the filter.
    
    Fix #161 
    
    # Checklist
    
    - [ ] Ran `./bin/test.sh` locally before merging

---
+++ src/Modules/getTreeDescendants.spec.lua
@@ -0,0 +1,31 @@
+return function()
+	local getTreeDescendants = require(script.Parent.getTreeDescendants)
+
+	it("should return an empty table when the root has no children", function()
+		local root = { name = "root", children = {} }
+
+		local result = getTreeDescendants(root)
+		expect(result).to.be.a("table")
+		expect(#result).to.equal(0)
+	end)
+
+	it("should return a table with all descendants when the root has children", function()
+		local child1 = { name = "child1", children = {} }
+		local child2 = { name = "child2", children = {} }
+		local root = { name = "root", children = { child1, child2 } }
+
+		local result = getTreeDescendants(root)
+		expect(result).to.be.a("table")
+		expect(#result).to.equal(2)
+	end)
+
+	it("should return a table with all descendants when the tree has multiple levels", function()
+		local grandchild = { name = "grandchild", children = {} }
+		local child = { name = "child", children = { grandchild } }
+		local root = { name = "root", children = { child } }
+
+		local result = getTreeDescendants(root)
+		expect(result).to.be.a("table")
+		expect(#result).to.equal(2)
+	end)
+end
