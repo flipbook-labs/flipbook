e90e4b4eb01a44e7fe1816a6191dca1e710d44f3
Marin Minnerly <me@vocksel.com>
2023-12-29 14:33:23 -0800

    Support both file extensions

--- src/Storybook/isStoryModule.lua
+++ src/Storybook/isStoryModule.lua
@@ -3,14 +3,11 @@ local flipbook = script:FindFirstAncestor("flipbook")
 local constants = require(flipbook.constants)
 
 local function isStoryModule(instance: Instance)
-	if constants.FLAG_ENABLE_COMPONENT_STORY_FORMAT then
-		if instance:IsA("ModuleScript") and instance.Name:match(constants.STORY_NAME_PATTERN_CSF) then
-			return true
-		end
-	else
-		if instance:IsA("ModuleScript") and instance.Name:match(constants.STORY_NAME_PATTERN) then
-			return true
-		end
+	if
+		instance:IsA("ModuleScript")
+		and (instance.Name:match(constants.STORY_NAME_PATTERN) or instance.Name:match(constants.STORY_NAME_PATTERN_CSF))
+	then
+		return true
 	end
 	return false
 end
