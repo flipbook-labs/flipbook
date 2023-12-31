9c88e8992968d2ed46e1f80e12b59289773dbf8c
Marin Minnerly <me@vocksel.com>
2023-12-31 12:45:00 -0800

    Fix stories.spec.lua

--- src/stories.spec.lua
+++ src/stories.spec.lua
@@ -2,22 +2,37 @@ local CoreGui = game:GetService("CoreGui")
 
 local flipbook = script:FindFirstAncestor("flipbook")
 
-local React = require(flipbook.Packages.React)
-local ReactRoblox = require(flipbook.Packages.ReactRoblox)
-local isStoryModule = require(flipbook.Storybook.isStoryModule)
+local ModuleLoader = require(flipbook.Packages.ModuleLoader)
+local loadStorybooksFromParent = require(flipbook.Storybook.loadStorybooksFromParent)
+local loadStoriesFromStorybook = require(flipbook.Storybook.loadStoriesFromStorybook)
 local mountStory = require(flipbook.Storybook.mountStory)
 
 return function()
-	for _, descendant in ipairs(flipbook:GetDescendants()) do
-		if isStoryModule(descendant) then
-			it("should mount/unmount " .. descendant.Name, function()
-				local story = require(descendant)
-				story.react = React
-				story.reactRoblox = ReactRoblox
+	local loader = ModuleLoader.new()
+	local storybooks, storybookErrors = loadStorybooksFromParent(flipbook, loader)
+
+	it("should load all storybooks", function()
+		expect(#storybookErrors).to.equal(0)
+	end)
+
+	for _, storybook in storybooks do
+		local stories, storyErrors = loadStoriesFromStorybook(storybook, loader)
+
+		it(`should load all stories for {storybook.name}`, function()
+			expect(#storyErrors).to.equal(0)
+		end)
+
+		for _, story in stories do
+			it(`should mount/unmount {storybook.name} > {story.name}`, function()
+				local controls = {}
+
+				if typeof(story) == "table" and story.controls then
+					controls = story.controls
+				end
 
 				local cleanup
 				expect(function()
-					cleanup = mountStory(story, story.controls, CoreGui)
+					cleanup = mountStory(story, controls, CoreGui)
 				end).to.never.throw()
 
 				if cleanup then
