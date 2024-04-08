44c6e1e4c4380642062d4cb3221ffb573dc3b244
Marin Minnerly <me@vocksel.com>
2024-04-07 20:58:47 -0700

    Install CSF from Wally

--- bin/wally-install.sh
+++ bin/wally-install.sh
@@ -4,8 +4,6 @@ set -e
 
 wally install
 
-git clone https://github.com/flipbook-labs/csf-lua Packages/CSF
-
 rojo sourcemap tests.project.json --output sourcemap.json
 
 wally-package-types --sourcemap sourcemap.json Packages/
