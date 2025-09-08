f3a384ecac88a2de48a2dbf7a923cc59bb55fe27
Marin Minnerly <me@vocksel.com>
2025-09-08 11:06:51 -0700

    Tidying (#331)

--- .github/workflows/validate-secret.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -z $2 ]]; then
-	echo "Could not find a secret named $1"
-	echo "Please set a repository secret named '$1' with"
-	exit 1
-else
-	echo "Found $1 secret. Continuing..."
-fi
